342a076e241828b786f122e4704afa122c25a2c8
orlando <orlando.nabil@gmail.com>
2022-09-06 23:09:05 -0500

    productDetails view

--- src/components/home/CardHome.jsx
+++ src/components/home/CardHome.jsx
@@ -1,27 +1,26 @@
 import { useNavigate } from 'react-router-dom'
 
-
-const CardHome = ({product}) => {
-
+const CardHome = ({ product }) => {
   const navigate = useNavigate()
 
-  const handleClick =() =>{
+  const handleClick = () => {
     navigate(`/product(${product.id})`)
-  };
-
+  }
 
   return (
-    <article className='shadow-lg hover:scale-105 duration-100 w-60 h-96 bg-white flex flex-col p-4 rounded-xl gap-6' onClick={handleClick}>
-      <header className='h-2/5 flex justify-center items-center'>
-        <img className='h-full' src={product.productImgs[0]} alt='' />
+    <article
+      className="shadow-lg hover:scale-105 duration-100 w-60 h-96 bg-white flex flex-col p-4 rounded-xl gap-6"
+      onClick={handleClick}>
+      <header className="h-2/5 flex justify-center items-center">
+        <img className="h-full" src={product.productImgs[0]} alt="" />
       </header>
-      <div className='h-3/5'>
-        <h3 className='card-home__name'>{product.title}</h3>
-        <section className='card-home__price'>
-          <h4 className='card-home__price-label'>Price</h4>
-          <span className='card-home__price-value'>{product.price}</span>
+      <div className="h-3/5">
+        <h3 className="card-home__name">{product.title}</h3>
+        <section className="card-home__price">
+          <h4 className="card-home__price-label">Price</h4>
+          <span className="card-home__price-value">{product.price}</span>
         </section>
-        <button className='card-home__btn'>Cart</button>
+        <button className="card-home__btn">Cart</button>
       </div>
     </article>
   )
